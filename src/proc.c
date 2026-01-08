5bfde804da406d4a859a811109503d6a24fe3bac
Joakim Fogelström <birdstream@hotmail.com>
2026-01-08 09:23:30 +0100

    Restore safe map lookup in proc parsing

--- src/proc.c
+++ src/proc.c
@@ -116,18 +116,19 @@ size_t proc_get_maps(pid_t pid, GHashTable* maps, GSet** exemaps) {
         size += length;
 
         if (maps || exemaps) {
-            preload_map_t probe;
-            preload_map_t* map = NULL;
+            gpointer orig_map;
+            preload_map_t* map;
+            gpointer value;
+
+            map = preload_map_new(file, offset, length);
 
             if (maps) {
-                probe.path = file;
-                probe.offset = offset;
-                probe.length = length;
-                map = g_hash_table_lookup(maps, &probe);
+                if (g_hash_table_lookup_extended(maps, map, &orig_map,
+                                                 &value)) {
+                    preload_map_free(map);
+                    map = (preload_map_t*)orig_map;
+                }
             }
-
-            if (!map)
-                map = preload_map_new(file, offset, length);
 
             if (exemaps) {
                 preload_exemap_t* exemap;
